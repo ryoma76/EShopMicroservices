61d760b28dcb68aded8fa2d4eb3e620ff3887fa5
ryoma76 <ryoma76@hotmail.it>
2025-04-15 11:42:01 +0200

    Aggiornamento ProductHandler con la nuova astrazione CQRS

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,15 +1,16 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
 
 namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) 
-    :IRequest<CreateProductResult>;
+    :ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler:IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler:ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        
         throw new NotImplementedException();
     }
 }
